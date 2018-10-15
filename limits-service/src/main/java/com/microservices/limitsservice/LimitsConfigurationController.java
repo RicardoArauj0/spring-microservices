d1ddcc8a4ce4c97bcdc0043404b6711882cb3ad2
ricardofranc.araujo@gmail.com <ricardofranc.araujo@gmail.com>
2018-10-15 17:04:07 -0300

    Centralized config with spring cloud config server

--- limits-service/src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,18 @@
 package com.microservices.limitsservice;
 
 import com.microservices.limitsservice.bean.LimitsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration config;
+
     @GetMapping("/limits")
     public LimitsConfiguration getLimitsFromConfiguration() {
-        return new LimitsConfiguration(1000, 1);
+        return new LimitsConfiguration(config.getMaximum(), config.getMinimum());
     }
 }
