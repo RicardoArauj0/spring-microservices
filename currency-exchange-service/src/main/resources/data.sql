b0744f2c3481d4e2a4379fff29fe4da140f4ef92
Ricardo Araujo <ricardofranc.araujo@gmail.com>
2018-10-24 16:10:14 -0300

    Configure JPA (Hibernate) and initializing data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple) values (1001, 'USD', 'BRL', 27);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple) values (1002, 'EUR', 'INR', 75);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple) values (1003, 'USD', 'AUD', 10);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple) values (1004, 'AUD', 'BRL', 32);
